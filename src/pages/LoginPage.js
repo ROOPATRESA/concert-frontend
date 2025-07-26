fcecc501251d1b6dc0d563dfa9fba7834d126ed6
roopa <roopatresa@gmail.com>
2025-07-26 11:21:18 +0530

    login api

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+const API = process.env.REACT_APP_API_URL || "http://localhost:3000/api";
+console.log("API URL →", API);
 
 function LoginPage() {
   const [email, setEmail] = useState("");
@@ -18,7 +20,7 @@ function LoginPage() {
     }
 
     try {
-      const response = await axios.post("http://localhost:3000/api/login_api", {
+      const response = await axios.post(`${API}/login_api`, {
         email,
         password,
       });
@@ -79,7 +81,7 @@ function LoginPage() {
           boxShadow: "0 8px 30px rgba(255, 223, 0, 0.4)",
         }}
       >
-        >
+        
         <h3
           className="text-center mb-4 text-warning fw-bold"
           style={{ letterSpacing: "1.5px" }}
